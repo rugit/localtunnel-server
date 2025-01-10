5f0d39d6fbbb7ac671e3b35076fbfd234cf6c357
Anupal Neog <neog.anupal101@gmail.com>
2025-01-10 13:45:38 +0530

    Add debug statements

--- server.js
+++ server.js
@@ -148,6 +148,7 @@ export default function (opt) {
     }
 
     const clientId = GetClientIdFromHostname(hostname);
+    debug("Client ID: %s", reqId);
     console.log(clientId);
     if (!clientId) {
       appCallback(req, res);
