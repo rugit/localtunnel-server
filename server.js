f478008dfa23994840dc51075d390d1352e4dcbe
Anupal Neog <neog.anupal101@gmail.com>
2025-01-10 13:33:51 +0530

    print clientId

--- server.js
+++ server.js
@@ -148,6 +148,7 @@ export default function (opt) {
     }
 
     const clientId = GetClientIdFromHostname(hostname);
+    print(clientId);
     if (!clientId) {
       appCallback(req, res);
       return;
